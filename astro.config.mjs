9b95b0b9a246d533a26d4f91c51ce66548c4405d
Brandon Ruben Vizcarra Tirado <branduski@MacBook-Air-de-Brandon.local>
2025-06-25 09:36:09 -0700

    Archivos del config actualizados

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
+import vercel from "@astrojs/vercel/serverless"; // or "@astrojs/vercel/edge" if you prefer edge
 
 export default defineConfig({
   integrations: [tailwind(), vue()],
-  output: "hybrid",
+  adapter: vercel(),
+  output: "server", // required for adapter to work
   server: {
     port: 4321,
   },
