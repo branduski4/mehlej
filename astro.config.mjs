72cedacdc9fa007ff467c7ba323120e5fd8c0dc3
Brandon Ruben Vizcarra Tirado <branduski@MacBook-Air-de-Brandon.local>
2025-06-25 11:00:29 -0700

    astro min js config file updated with vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
-import vercel from "@astrojs/vercel/serverless"; // or "@astrojs/vercel/edge" if you prefer edge
+import vercel from "@astrojs/vercel/static";
 
 export default defineConfig({
   integrations: [tailwind(), vue()],
   adapter: vercel(),
-  output: "server", // required for adapter to work
+  output: "static",
   server: {
     port: 4321,
   },
